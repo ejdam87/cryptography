6917fc64a8b92d1771a7a4cc2d5164d37eb9aa78
ejdam87 <adam.dzadon@gmail.com>
2023-02-27 17:37:14 +0100

    Finish caesar

--- symetric/ceasar.c
+++ symetric/ceasar.c
@@ -1,13 +1,13 @@
 #include "ceasar.h"
 #include "alphabet.h"
 
+#include <string.h>
 
 char caesar_encrypt( char ch, int k )
 {
     char pos = ch - 'a'; // number from 0 to 25
     char shifted = ( pos + k ) % ALPHABET_LENGTH;
     return shifted + 'a';
-
 }
 
 char caesar_de_encrypt( char enc, int k )
@@ -21,7 +21,18 @@ char caesar_de_encrypt( char enc, int k )
     return shifted + 'a';
 }
 
-void caesar_string_encrypt( char* str, char k )
+void caesar_string_encrypt( char* str, char** res, char k )
 {
+    for( int i = 0; i < strlen( str ); i++ )
+    {
+        (*res)[ i ] = caesar_encrypt( str[ i ], k );
+    }
+}
 
+void caesar_string_de_encrypt( char* str, char** res, char k )
+{
+    for( int i = 0; i < strlen( str ); i++ )
+    {
+        (*res)[ i ] = caesar_de_encrypt( str[ i ], k );
+    }
 }
--- symetric/ceasar.h
+++ symetric/ceasar.h
@@ -4,4 +4,7 @@
 char caesar_encrypt( char ch, int k );       // shifts character 'ch' by 'k' places
 char caesar_de_encrypt( char enc, int k );   // inverse operation to encryption
 
+void caesar_string_encrypt( char* str, char** res, char k );    // encrypts 'str' to 'res' char by char
+void caesar_string_de_encrypt( char* str, char** res, char k ); // inverse to encrypytion
+
 #endif //SYMETRIC_CEASAR_H
